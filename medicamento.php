e9d006237a23066a7fda4209db1ca3d759ac7dce
Clinton Hudson <clintonhudson01@hotmail.com>
2021-06-05 01:13:22 -0400

    Modificação na pagina de medicamento

--- medicamento.php
+++ medicamento.php
@@ -253,8 +253,8 @@ if (!isset($_SESSION['usuario'])) {
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="js/jquery-3.5.1.slim.min.js"></script>
-        <script src="js/popper.min.js"></script>
+        <!--      <script src="js/jquery-3.5.1.slim.min.js"></script>  
+        <script src="js/popper.min.js"></script> -->
         <script src="js/bootstrap.min.js"></script>
 
     </body>
